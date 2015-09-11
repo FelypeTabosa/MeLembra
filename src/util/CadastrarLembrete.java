b4d51e334a8c1a64bf1f3882822930fbc399fa95
Felype Tabosa <felypetabosa@gmail.com>
2015-09-11 09:52:13 -0300

    closed #1
    closed #1

--- src/util/CadastrarLembrete.java
+++ src/util/CadastrarLembrete.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class CadastrarLembrete
+ * Servlet implementation class CadastrarLembrete.
  */
 @WebServlet("/CadastrarLembrete")
 public class CadastrarLembrete extends HttpServlet {
